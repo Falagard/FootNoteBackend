8a6caeaaab1e8a650252f2b328bbc9141908f560
Clay Larabie <clay@alumind.com.au>
2025-09-19 03:50:45 +1000

    updates to try to get static content serving working

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 "# FootNoteBackend" 
 
-haxelib install tink_web 
-haxelib install hxnodejs
 
-haxe -js server.js -lib hxnodejs -lib tink_web -main Server
+haxelib install hxnodejs
+haxelib install mime
+
+git clone https://github.com/haxetink/tink_http
+
+haxelib dev tink_http tink_http
+
+git clone https://github.com/haxetink/tink_http_middleware
+
+haxelib dev tink_http_middleware tink_http_middleware
+
+git clone https://github.com/haxetink/tink_core
+
+haxelib dev tink_core tink_core
+
+git clone https://github.com/benmerckx/asys
+
+haxelib dev asys asys
+
+haxe -js server.js -lib hxnodejs -lib tink_web -lib tink_http_middleware -main Server
 
 node server.js
--- Server.hx
+++ Server.hx
@@ -1,21 +1,27 @@
 import tink.http.containers.*;
+import tink.http.Handler;
 import tink.http.Response;
+import tink.http.middleware.Static;
 import tink.web.routing.*;
 
 class Server {
     static function main() {
-        var container = new NodeContainer(8080); 
-        var router = new Router<Root>(new Root());
-        container.run(function(req) {
-            return router.route(Context.ofRequest(req))
-                .recover(OutgoingResponse.reportError);
-        });
+        var container = new NodeContainer(8080);
+        var router = new Router<Api>(new Api());
+        var handler:Handler = req -> router.route(Context.ofRequest(req)).recover(OutgoingResponse.reportError);
+        container.run(handler.applyMiddleware(new Static('public_html', '/static')));
     }
+}
+
+class Api {
+    public function new() {}
+
+    @:sub public var api:Root = new Root();
 }
 
 class Root {
     public function new() {}
-    
+
     @:get('/')
     @:get('/$name')
     public function hello(name = 'World')
